7cf10c8563ee63d609e2fab274613ebc3a4ccb24
sandeeperanti <sandeep@gmx.ru>
2023-05-30 11:34:57 +0530

    add formatter

--- priv/repo/migrations/20230519132342_create_user_table.exs
+++ priv/repo/migrations/20230519132342_create_user_table.exs
@@ -2,11 +2,10 @@ defmodule Guess.Repo.Migrations.CreateUserTable do
   use Ecto.Migration
 
   def change do
-
     create table(:users) do
-      add :name, :string
-      add :email, :string
-      add :gamecount, :integer
+      add(:name, :string)
+      add(:email, :string)
+      add(:gamecount, :integer)
 
       timestamps()
     end
--- priv/repo/migrations/20230522075505_create_game_table.exs
+++ priv/repo/migrations/20230522075505_create_game_table.exs
@@ -1,16 +1,13 @@
 defmodule Guess.Repo.Migrations.CreateGameTable do
   use Ecto.Migration
 
-
   def change do
     create table(:games) do
-      add :user_id, references(:users), null: false
-      add :rn, :integer
-      add :trailcount, :integer
-
+      add(:user_id, references(:users), null: false)
+      add(:rn, :integer)
+      add(:trailcount, :integer)
 
       timestamps()
-
     end
   end
 end
