d65bbb2d14991d1ed47d1f447c1b4b3b4e8e7650
sandeeperanti <sandeep@gmx.ru>
2023-06-16 17:37:45 +0530

    reorg

--- lib/guess/user_details.ex
+++ lib/guess/user_details.ex
@@ -6,11 +6,11 @@ defmodule Guess.UserDetails do
     validate_name(name, n)
   end
 
-  def validate_name(name, n) do
-    count = String.length(name)
+  def validate_name(namex, nx) do
+    count = String.length(namex)
 
-    if count >= n do
-      name
+    if count >= nx do
+      namex
     else
       get_name()
     end
@@ -22,12 +22,12 @@ defmodule Guess.UserDetails do
     validate_email(email)
   end
 
-  def validate_email(email) do
+  def validate_email(mail) do
     validformat =
       ~r/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-    if Regex.match?(validformat, email) do
-      email
+    if Regex.match?(validformat, mail) do
+      mail
     else
       get_email()
     end
